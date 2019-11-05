c55273432715c74e83012773ae62e664509eeffe
Mark Seale <camseale5@yahoo.com>
2019-11-04 23:13:09 -0600

    Validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,11 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+   @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
 
   def show
@@ -31,10 +34,13 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
-    @place.update_attributes(place_params)
+  @place.update_attributes(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
   end
-
+end
 def destroy
   @place = Place.find(params[:id])
   @place.destroy
@@ -46,4 +52,5 @@ def destroy
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
+end
 end
