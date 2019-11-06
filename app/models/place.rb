3ccab9fd44209d8c4dbf62c285184206ceb24e42
Mark Seale <camseale5@yahoo.com>
2019-11-06 01:24:52 -0600

    Comments in database

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
-  
+
   validates :name, :address, :description, length: { minimum:3 }, presence: true
 
 end
