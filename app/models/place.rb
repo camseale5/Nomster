745ba8d8a2de48c6bd2f1aa9fcb68f1a8e0e74fa
Mark Seale <camseale5@yahoo.com>
2019-11-06 06:14:39 +0000

    Geocoding address

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  geocoded_by :address
+  after_validation :geocode
+  
+  validates :name, :address, :description, length: { minimum:3 }, presence: true
+
 end
